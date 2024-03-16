64d249c4b9d051407253a23fd36775fba794dcba
mingderwang <admin@ming-ders-MacBook.local>
2024-03-16 16:48:56 +0800

    tmp

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
-import { lightAccountClient, smartAccountClient } from "./smartAccountClient";
+import { LocalAccountSigner, SmartAccountSigner, polygonMumbai } from "@alchemy/aa-core";
+import { lightAccountClient } from "./smartAccountClient";
+import { createLightAccountClient, lightAccountClientActions } from "@alchemy/aa-accounts";
+import { custom } from "viem";
 
-// get on-chain account owner address
-const smartAccountAddress = smartAccountClient.getAddress();
-const lightAccountAddress = lightAccountClient.getAddress();
+const NEW_OWNER_MNEMONIC = `test `.repeat(11) + `junk`;
+const newOwner: SmartAccountSigner = LocalAccountSigner.mnemonicToAccountSigner(NEW_OWNER_MNEMONIC);
+const accountAddress = lightAccountClient.getAddress();
+const oldAccountClient = lightAccountClient.extend(lightAccountClientActions);
+const chain = polygonMumbai;
+(async () => {
+  const hash = await oldAccountClient.transferOwnership({
+    newOwner,
+    waitForTxn: true,
+  });
 
-console.log(smartAccountClient);
-console.log(`smartAccountClint: ${smartAccountAddress}`);
-console.log(lightAccountClient);
-console.log(`lightAccountClint: ${lightAccountAddress}`);
+  console.log(hash);
+
+  const transferredClient = await createLightAccountClient({
+    transport: custom(oldAccountClient),
+    chain,
+    owner: newOwner, // NOTE: you MUST to specify the original smart account address to connect using the new owner/signer
+  });
+
+  console.log(transferredClient.getAddress());
+
+})();
